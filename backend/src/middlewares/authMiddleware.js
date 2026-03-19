6527925a570c691d428dbf89a0009a58b18cd90f
prathabhattarai <prathabhattarai2@gmail.com>
2026-03-19 21:36:50 +0545

    security: construct strict JSON Web Token middleware

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/env');
+const db = require('../models');
+
+const authenticate = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, jwtSecret);
+    const user = await db.User.findByPk(decoded.id);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Unauthorized', error: err.message });
+  }
+};
+
+module.exports = authenticate;
