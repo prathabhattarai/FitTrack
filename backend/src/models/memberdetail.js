997f55d52071b6b4558de90d5821693dffd02af9
prathabhattarai <prathabhattarai2@gmail.com>
2026-03-19 21:36:51 +0545

    feat: formulate MemberDetail expansion model

---
+++ backend/src/models/memberdetail.js
@@ -0,0 +1,21 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class MemberDetail extends Model {
+    static associate(models) {
+      MemberDetail.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+      MemberDetail.belongsTo(models.MembershipPlan, { foreignKey: 'active_plan_id', as: 'activePlan' });
+    }
+  }
+  MemberDetail.init({
+    user_id: { type: DataTypes.INTEGER, allowNull: false },
+    phone: DataTypes.STRING,
+    address: DataTypes.TEXT,
+    joined_date: DataTypes.DATE,
+    active_plan_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'MemberDetail',
+  });
+  return MemberDetail;
+};
