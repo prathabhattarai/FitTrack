60c47a07d0df208595202c61bd5d61768617ebbc
prathabhattarai <prathabhattarai2@gmail.com>
2026-03-19 21:36:50 +0545

    chore: attach db parameters to sequelize config

---
+++ backend/src/config/database.js
@@ -0,0 +1,28 @@
+const env = require("./env");
+
+module.exports = {
+  development: {
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.name,
+    host: env.db.host,
+    dialect: "mysql",
+    logging: false
+  },
+  test: {
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.name + "_test",
+    host: env.db.host,
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    username: env.db.user,
+    password: env.db.password,
+    database: env.db.name,
+    host: env.db.host,
+    dialect: "mysql",
+    logging: false
+  }
+};
